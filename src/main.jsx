1493ee1d25c48d27caa09a2a1ccc552fba72582e
gugeyro <guvanch2000@gmail.com>
2025-06-21 13:59:25 +0500

    Очистка main.jsx, только нужные импорты

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './style.css'; // только Tailwind
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
